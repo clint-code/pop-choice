5c2b65a9e91a477f5bdbbb0416b3486db2c4c9ba
Clint Code <mutinda1993@gmail.com>
2026-08-19 15:42:51 +0300

    Updated routes

--- src/router.jsx
+++ src/router.jsx
@@ -1,22 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
-import MovieRecommendations from "./MovieRecommendations";
 import StretchGoal from "./StretchGoal";
 
 export const router = createBrowserRouter([
 
     {
         path: '/',
-        element: <App />
-    },
-
-    {
-        path: '/pop-choice-2.0',
         element: <StretchGoal />
     },
 
-    {
-        path: '/movie-recommendations',
-        element: <MovieRecommendations />
-    }
 ]);
